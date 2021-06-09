32db480a11caceff647a43fbfba8627da2685be0
rexlin <rexlin6245@gmail.com>
2021-06-09 14:36:05 -0700

    change for push to heroku

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/restaurantdatas'
 
-mongoose.connect('mongodb://localhost/restaurantdatas', {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
