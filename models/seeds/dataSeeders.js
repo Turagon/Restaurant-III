2793bbe0af8abe621f5d4caf844cb736700856f4
rexlin <rexlin6245@gmail.com>
2021-07-07 13:09:49 -0700

    add dataseeder.js to build seed data, add user data

--- models/seeds/dataSeeders.js
+++ models/seeds/dataSeeders.js
@@ -1,20 +1,70 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const restaurantData = require('../restaurantData')
+const user = require('../userData')
 const db = require('../../config/mongoose')
 const seedData = require('../../restaurant.json').results
+const seedUser = require('../../userData.json').users
+const bcrypt = require('bcryptjs')
 
 db.once('open', () => {
-  for (let i of seedData) {
-    restaurantData.create({
-      name: i.name,
-      name_en: i.name_en,
-      category: i.category,
-      image: i.image,
-      location: i.location,
-      phone: i.phone,
-      rating: i.rating,
-      description: i.description,
-      collected: false
+  seedUser.forEach(item => {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(item.password, salt, (err, hash) => {
+        return user.create({
+          name: item.name,
+          email: item.email,
+          password: hash
+        })
+        .then(user => {
+          const userName = user.name
+          const refId = user._id
+          for (let i of seedData) {
+            if (userName === 'user1' && Number(i.id) <= 3) {
+              restaurantData.create({
+                name: i.name,
+                name_en: i.name_en,
+                category: i.category,
+                image: i.image,
+                location: i.location,
+                phone: i.phone,
+                rating: i.rating,
+                description: i.description,
+                collected: false,
+                userId: refId
+              })
+            } else if (userName === 'user2' && Number(i.id) > 3 && Number(i.id) <= 6) {
+              restaurantData.create({
+                name: i.name,
+                name_en: i.name_en,
+                category: i.category,
+                image: i.image,
+                location: i.location,
+                phone: i.phone,
+                rating: i.rating,
+                description: i.description,
+                collected: false,
+                userId: refId
+              })
+            } else if (userName === 'user3' && Number(i.id) > 6) {
+              restaurantData.create({
+                name: i.name,
+                name_en: i.name_en,
+                category: i.category,
+                image: i.image,
+                location: i.location,
+                phone: i.phone,
+                rating: i.rating,
+                description: i.description,
+                collected: false,
+                userId: refId
+              })
+            }
+          }
+        })
+      })
     })
-  }
+  })
   console.log('done')
 })
